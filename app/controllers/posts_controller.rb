badc0e3f550ab00473f32cf8ed0d18c998cb1730
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 19:21:50 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,17 +21,17 @@ class PostsController < ApplicationController
   def edit
   end
 
-  def body
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
-
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
   end
 
   def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
+  def body
     post = Post.find(params[:id])
     render json: PostSerializer.serialize(post)
   end
