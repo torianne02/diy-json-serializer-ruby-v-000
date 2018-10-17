32fe313da9e5d54f73d835d223659b5c5addbfa6
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-10-17 19:35:56 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
-  def body
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
-
   def index
     @posts = Post.all
   end
@@ -29,6 +24,11 @@ class PostsController < ApplicationController
   def update
     @post.update(post_params)
     redirect_to post_path(@post)
+  end
+
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
   end
 
 private
